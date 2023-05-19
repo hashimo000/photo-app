b5c7d270f993bdbb2bd847f22ed70f907ab327c3
hashimon000 <hashimon.hashimon@icloud.com>
2023-05-19 10:21:39 +0900

    index.js編集

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { App } from './App'
+
+
+const root = document.getElementById('root')
+const reactRoot = ReactDOM.createRoot(root)
+reactRoot.render(<App />)
