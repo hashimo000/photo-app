229cb3295ec5f5e903c277b884ef2acb4d6a2dc5
hashimon000 <hashimon.hashimon@icloud.com>
2023-05-19 15:29:44 +0900

    buttonを押したら画像が変わる

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React ,{useState}from "react";
 
 export const App=()=>{
  
- const[dog,setDog]=useState("")
+ const[dog,setDog]=useState("https://images.dog.ceo/breeds/hound-english/n02089973_2551.jpg")
+ 
  const dograndom =async()=>{
   const res =await fetch('https://dog.ceo/api/breeds/image/random')
   const data =await res.json();
@@ -14,7 +15,7 @@ export const App=()=>{
   <div>
     <header>犬の画像で癒されよう！！</header>
     <div>犬の画像</div>
-    <img src="https://images.dog.ceo/breeds/hound-english/n02089973_2551.jpg" alt="犬の画像"></img>
+    <img src={dog} alt="犬の画像"></img>
 
     <br></br><button onClick ={dograndom}>他の犬を探す！</button>
 
