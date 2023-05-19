e79a5ea3d5756a7811605662110b2702f44667ed
hashimon000 <hashimon.hashimon@icloud.com>
2023-05-19 10:26:57 +0900

    altとbuttonタグの設置

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,12 @@ export const App=()=>{
   <div>
     <header>犬の画像で癒されよう！！</header>
     <div>犬の画像</div>
-    <img src="https://images.dog.ceo/breeds/hound-english/n02089973_2551.jpg"></img>
+    <img src="https://images.dog.ceo/breeds/hound-english/n02089973_2551.jpg" alt="犬の画像"></img>
+
+  <button>他の犬を探す！</button>
+
   </div>
+
 )};
 
 export default App;
