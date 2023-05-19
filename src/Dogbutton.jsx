95884ee2e6720347834dfdeb3c8a7f339853e255
hashimon000 <hashimon.hashimon@icloud.com>
2023-05-19 16:25:03 +0900

    ReactBootstrapの実装

--- src/Dogbutton.jsx
+++ src/Dogbutton.jsx
@@ -1,7 +1,7 @@
 import React ,{useState} from "react";
 import Dogphoto from "./Dogphoto";
-
-
+import Button from 'react-bootstrap/Button';
+import "./index.css"
 export const Dogbutton=()=>{
   const[dog,setDog]=useState("https://images.dog.ceo/breeds/hound-english/n02089973_2551.jpg")
  
@@ -16,7 +16,7 @@ export const Dogbutton=()=>{
     return(
         <div>
             <Dogphoto dog={dog}/>
-            <br></br><button onClick ={dograndom}>他の犬を探す！</button>
+            <Button variant="outline-primary" onClick ={dograndom}>他の犬を探す！</Button>
 
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { App } from './App'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = document.getElementById('root')
 const reactRoot = ReactDOM.createRoot(root)
